cf18b3e7cd98ba3ce0fbc065c8c6eee2e925fbe4
Levi Caro <92340774+LeviCaro@users.noreply.github.com>
2021-11-05 22:31:36 -0700

    Add files via upload

---
+++ EVA2_1_For_Recursivo.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author Daniel Levi Caro Alvarado
+ */
+package eva2_1_list_queue_stack;
+
+
+public class EVA2_1_For_Recursivo {
+
+    public static void main(String[] args) {
+        
+        recursivo();
+        
+    }
+    
+    public static void recursivo(){
+        System.out.println("Hola Mundo");
+        recursivo();
+    }
+    
+}
