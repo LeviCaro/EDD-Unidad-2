6d97787380caa1648366611c81379447880f9b3d
Levi Caro <92340774+LeviCaro@users.noreply.github.com>
2021-11-05 22:32:31 -0700

    Add files via upload

---
+++ EVA2_5_FIBONACCI.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author Daniel Levi Caro Alvarado
+ */
+package eva2_1_list_queue_stack;
+
+
+public class EVA2_5_FIBONACCI {
+
+    public static void main(String[] args) {
+        
+    }
+    
+    public static int fibonacci(int iPos){
+        return 0;
+        
+    }
+    
+}
